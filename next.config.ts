913b2a1150d610fba7f463d865908d0266d7edb9
rokoperki <roko.perisic@gmail.com>
2025-07-21 12:39:14 +0200

    icons fix

--- next.config.ts
+++ next.config.ts
@@ -5,19 +5,6 @@ const nextConfig: NextConfig = {
   images: {
     domains: ["digital-waiter-service.s3.eu-north-1.amazonaws.com"],
   },
-  webpack(config: {
-    module: { rules: { test: RegExp; use: string[] }[] };
-    resolve: { fallback: Record<string, boolean> };
-  }) {
-    config.module.rules.push({
-      test: /\.svg$/,
-      use: ['@svgr/webpack'],
-    });
-
-    config.resolve.fallback = { fs: false, path: false };
-
-    return config;
-  },
 };
 
 export default nextConfig;
--- src/assets/icons/index.ts
+++ src/assets/icons/index.ts
@@ -1,9 +1,9 @@
-export { default as GlobeSvg } from "./globe.svg";
+export { default as CartSvg } from "./cart.svg";
 export { default as SearchSvg } from "./search.svg";
-export { default as DotVerticalFilledSvg } from "./dot-vertical-filled.svg";
-export { default as PlusSvg } from "./plus.svg";
-export { default as HouseSvg } from "./house.svg";
-export { default as ClockSvg } from "./clock.svg";
 export { default as CloseSvg } from "./close.svg";
 export { default as ArrowLeftSvg } from "./arrow-left.svg";
-export { default as CartSvg } from "./cart.svg";
+export { default as PlusSvg } from "./plus.svg";
+export { default as GlobeSvg } from "./globe.svg";
+export { default as DotVerticalFilledSvg } from "./dot-vertical-filled.svg";
+export { default as ClockSvg } from "./clock.svg";
+export { default as HouseSvg } from "./house.svg";
